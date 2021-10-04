e393c4ad674c17d42d9c409d2955bace9571ea53
kihore <91452903+kihore@users.noreply.github.com>
2021-10-04 17:27:16 +0530

    Add files via upload

---
+++ crud/app.js
@@ -0,0 +1,127 @@
+var selectedRow = null
+
+function onFormSubmit() {
+    
+        var formData = readFormData();
+        if (selectedRow == null){
+            insertNewRecord(formData);
+        }
+        else {
+            updateRecord(formData);
+        }
+        resetForm();
+        
+}
+
+function readFormData() {
+    var formData = {};
+    formData["Name"] = document.getElementById("Name").value;
+    formData["surname"] = document.getElementById("surname").value;
+    formData["gender"]= document.querySelector('input[name=gender]:checked').value;
+    formData["phone"] = document.getElementById("phone").value;
+    formData["hobby"] = document.getElementById("hobby").value;
+    return formData;
+}
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.Name;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.surname;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.phone;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.gender;
+    cell5 = newRow.insertCell(4);
+    cell5.innerHTML = data.hobby;
+    cell5 = newRow.insertCell();
+    cell5.innerHTML =  `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("Name").value = "";
+    document.getElementById("surname").value = "";
+    document.getElementById("phone").value = "";
+    var gender = document.getElementsByClassName("gender");
+    for(index = 0; index<gender.length; index++)
+        gender[index].checked = false;
+    document.getElementById("hobby").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("Name").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("surname").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("phone").value = selectedRow.cells[2].innerHTML;
+    var gender = selectedRow.cells[3].innerHTML;
+    if(gender=="Male")
+        document.getElementById("male").checked=true;
+    else if(gender=='female') {
+        document.getElementById("female").checked=true;
+    } else {
+        document.getElementById("others").checked=true;
+    }
+    document.getElementById("hobby").value = selectedRow.cells[4].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.Name;
+    selectedRow.cells[1].innerHTML = formData.surname;
+    selectedRow.cells[2].innerHTML = formData.phone;
+    selectedRow.cells[3].innerHTML = formData.gender;
+    selectedRow.cells[4].innerHTML = formData.hobby;
+}
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+      resetForm();
+    }
+}
+function isphone(evt) {
+    var iKeyCode = (evt.which) ? evt.which : evt.keyCode
+    if (iKeyCode != 46 && iKeyCode > 31 && (iKeyCode < 48 || iKeyCode > 57))
+        return false;
+
+    return true;
+}
+function myFunction() {
+   
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("employeeList");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
